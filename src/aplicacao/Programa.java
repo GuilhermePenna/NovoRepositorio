eede9f2bdbe3361e1fce66b5ca0ab07da8940b87
Guilherme Penna <g_henrique1998@hotmail.com>
2020-05-23 16:23:03 -0300

    acrescentada mensagem projeto foi alterado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -3,8 +3,9 @@ package aplicacao;
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		
+		System.out.println("O projeto foi alterado!");
 	}
 
 }
